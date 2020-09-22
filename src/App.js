a7558e2ac64963278d4bb16cba59cc0192f2fb76
Moses Gameli <moses.qanosa@gmail.com>
2020-09-22 18:05:17 +0000

    weather app

--- src/App.js
+++ src/App.js
@@ -1,24 +1,54 @@
 import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+
+const id = 'c1e02e8db36a1accbeb63619c7d1d6e8'
+const website = 'https://api.openweathermap.org/data/2.5/'
 
 function App() {
+    const [ query, setQuery ] = React.useState('');
+    const [ weather, setWeather ] = React.useState({});
+
+    const handleSearch = (e) => {
+      e.preventDefault();
+
+      fetch(`${website}weather?q=${query}&units=metric&APPID=${id}`)
+      .then((res) => res.json())
+      .then((result) => {
+          setQuery("");
+          setWeather(result);
+          console.log(result);
+      });
+    }
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div className='search-bar'>
+        <input
+          type='search'
+          value={query}
+          placeholder='search...'
+          onChange={ e => setQuery(e.target.value)}
+        />
+        <input
+          type='submit'
+          value='Search'
+          onClick={handleSearch}
+        />
+      </div>
+      {typeof weather.main != "undefined" ? (
+          <div className="">
+            <div className="">
+              <span>{weather.name}, {weather.sys.country} </span>
+            </div>
+            <div className="">
+              <div className="">
+                <h3> temperature <br/> <span>{Math.round(weather.main.temp)}°C</span> </h3>
+              </div>
+              <div className="">
+                <h3> weather <br/> <span>{weather.weather[0].main}</span> </h3>
+              </div>
+            </div>
+          </div>
+        ) : null }
     </div>
   );
 }
